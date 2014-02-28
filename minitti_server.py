fd7f7fb4317a0fa9622c57e75ca7e05c325c231f
tjlane <thomas.joseph.lane@gmail.com>
2014-02-27 20:30:57 -0800

    work work

--- minitti_server.py
+++ minitti_server.py
@@ -12,39 +12,105 @@ import zmq
 from psdata import ImageData
 from psdata import XYPlotData
 
-import utilities
+from utilities import *
 
-################################################################################
+# ARGUMENT PARSING
+parser = argparse.ArgumentParser(description='Psana plot server application')
+
+parser.add_argument(
+    '-p',
+    '--port',
+    metavar='PORT',
+    type=int,
+    default=5556,
+    help='the tcp port to use on the server'
+)
+
+parser.add_argument(
+    '-g',
+    '--geometry',
+    type=str,
+    default='default_geometry.npy',
+    help='A numpy array on disk specifying the q-values for each bin'
+)
+
+parser.add_argument(
+    '-m',
+    '--mask',
+    type=str,
+    default='default_mask.npy',
+    help='A numpy array on disk specifying the masked pixels'
+)
+
+parser.add_argument(
+    '-r',
+    '--run',
+    metavar='aspect',
+    type=int,
+    default=-1,
+    help='Which run to analyze. If no parameter specified (default), attempt to \
+          lock onto the shared memory data stream.'
+)
+
+args = parser.parse_args()
+
+print ''
+print '*' * 80
+print ''
+print '\t\t MINITTI BEAMTIME SERVER -- PEW PEW PEW'
+print ''
+print '*' * 80
+print ''
+
 # ZMQ SETUP 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.setsockopt(zmq.SNDHWM, 10)
-socket.bind("tcp://*:%d" % 12322)
-print "Broadcasting ZMQ on port 12322"
-################################################################################
+socket.bind("tcp://*:%d" % args.port)
+print "Broadcasting via ZMQ on port: %d" % args.port
 
-# ------- CONFIG -------
 # PSANA CONFIG FILE - this must be set before a datasource is created
 basedir = os.path.split(os.path.abspath( __file__ ))[0]
 config_fn = os.path.join(basedir, "minitti.cfg")
 psana.setConfigFile(config_fn)
-psana.setOption('psana.l3t-accept-only',0) 
+psana.setOption('psana.l3t-accept-only',0)
+print "Loading psana config file:    %s" % config_fn
+
+# Aquire the geometry and mask
+print "Loading geometry from:        %s" % args.geometry
+geometry = np.load(args.geometry)
+print "Loading pixel mask from:      %s" % args.mask
+mask = np.load(args.mask)
 
 # Define experiment, run. Shared memory syntax (for SXR): shmem=0_1_SXR.0:stop=no
-ds = psana.DataSource('exp=xppi0613:run=275') 
+if args.run >= 0:
+    print "\nLocking on to shared memory..."
+    ds = psana.DataSource('exp=xppi0613:run=275')
+elif args.run == -1:
+    print "\nLocking on to shared memory..."
+    ds = psana.DataSource('shmem=0_1_SXR.0:stop=no')
+else:
+    raise ValueError('`run` parameter must either be an int corresponding to a '
+                     'run, or the keyword "online"')
 
 cspad_src  = psana.Source('DetInfo(XppGon.0:Cspad.0)')
 evr_src    = psana.Source('DetInfo(NoDetector.0:Evr.0)')
-FEEGas_src = psana.Source('BldInfo(FEEGasDetEnergy)')
 
-status_frequency      = 100         # print a status every on the Nth shot
-plot_update_frequency = 100         # carry out radial integration and update plots every N good shots
+plot_update_frequency = 10
 
 
 # ------ END CONFIG ---------
 
 epics = ds.env().epicsStore()
 calib = ds.env().calibStore()
+
+laser_on  = np.zeros((32, 185, 194*2))
+laser_off = np.zeros((32, 185, 194*2))
+
+n_laser_on_shots  = 0
+n_laser_off_shots = 0
+
+shot_result = 'no data'
 
 for i,evt in enumerate(ds.events()):
     
@@ -58,26 +124,53 @@ for i,evt in enumerate(ds.events()):
     # extract useful data
     raw_image       = np.vstack([ cspad.quads(i).data() for i in range(4) ])
     corrected_image = np.vstack([ corrected_cspad.quads(i).data() for i in range(4) ])
-    assembled_image = evt.get(psana.ndarray_float32_2, cspad_src, 'assembled_image')
 
-    print raw_image.shape
-    print corrected_iamge.shape
-    print assembled_image.shape
+    assert raw_image.shape == (32, 185, 194*2)
+    assert corrected_image.shape == (32, 185, 194*2)
 
     xfel_status, uv_status = pumpprob_status(EVR_code)
+    
+    # === filter for bad shots ===
+    
+    # filter 
+    if corrected_image.sum() > threshold:
+        print "-- bad shot"
+        continue
+    
+    # === accumulate the results ===
+
+    if (xfel_status and uv_status):       # good data, w/ pump
+        shot_result = 'uv & xfel on'
+        n_laser_on_shots += 1
+        update_average(n_laser_on_shots, laser_on, corrected_image)
+        
+    elif (xfel_status and not uv_status): # good data, no pump
+        shot_result = 'uv off, xfel on'
+        n_laser_off_shots += 1
+        update_average(n_laser_off_shots, laser_off, corrected_image)
+        
+    else:                                 # no scattering data...
+        shot_result = 'xfel off'
 
 
+    # === compute radial averages ===
+    evt_rad     = radial_average(corrected_image, geometry, mask)
+    avg_rad_on  = radial_average(laser_on, geometry, mask)
+    avg_rad_off = radial_average(laser_off, geometry, mask)
 
     # ==============================
     # PUBLISH DATA VIA ZMQ    
     # ==============================
-    avgImage = ImageData(goodEvents, "AVERAGE OF ALL CALIBRATED CSPAD", avgCspadAll)
-    socket.send("avgcspad", zmq.SNDMORE)
-    socket.send_pyobj(avgImage)
+    rad_avg_plot = XYPlotData(goodEvents, "I(q) Laser On & Off",
+                              radialPointsAvg, angularIntegAvg)
+    socket.send("rad-avg", zmq.SNDMORE)
+    socket.send_pyobj(rad_avg_plot)
+    
+    
+    rad_diff_plot = XYPlotData(goodEvents, "I(q) Laser On/Off Difference",
+                               radialPointsAvg, angularIntegAvg)
+    socket.send("rad-diff", zmq.SNDMORE)
+    socket.send_pyobj(rad_diff_plot)
 
-    avgRadIntegPlot = XYPlotData(goodEvents, "RADIAL INTEGRATION OF AVERAGE CSPAD", \
-                                 radialPointsAvg,angularIntegAvg)
-    socket.send("radintegavg", zmq.SNDMORE)
-    socket.send_pyobj(avgRadIntegPlot)
 
-
+    print "Run: %d | Shot %d | %s" % (args.run, i, shot_result)
--- utilities.py
+++ utilities.py
@@ -36,19 +36,64 @@ def pumpprobe_status(evr_data):
     # uv off & x-ray on: 41, 67, 141
     # all dark: 162, 163
     # return is (xfel_status, uv_status)
-    evr_legend = {140 : (1, 1), # uv on/xray on
-                  41  : (1, 0), # uv off/xray on
-                  67  : (1, 0),
-                  141 : (1, 0),
-                  162 : (0, 0), # all dark
-                  163 : (0, 0)} 
-                  
-
+    # evr_legend = {140 : (1, 1), # uv on/xray on
+    #               41  : (1, 0), # uv off/xray on
+    #               67  : (1, 0),
+    #               141 : (1, 0),
+    #               162 : (0, 0), # all dark
+    #               163 : (0, 0)} 
+    #               
+    
     xfel_status, uv_status = (None, None)
     for fifoEvent in evr_data.fifoEvents():
         if fifoEvent.eventCode() in evr_legend.keys():
             xfel_status, uv_status = evr_legend[fifoEvent.eventCode()]
             print "\tEVR EVENT: %d || XRAY: %d || UV: %d" % (fifoEvent.eventCode(), xfel_status, uv_status)
 
-
     return xfel_status, uv_status
+    
+    
+def radial_average(image, q_values, mask, n_bins=100):
+    """
+    Bin pixel intensities by their momentum transfer.
+
+    Parameters
+    ----------            
+    image : np.ndarray
+        The intensity at each pixel, same shape as pixel_pos
+    q_values : np.ndarray (float)
+        For each pixel, this is the momentum transfer value of that pixel
+    mask : np.ndarray (int)
+        A boolean (int) saying if each pixel is masked or not
+    n_bins : int
+        The number of bins to employ. If `None` guesses a good value.
+
+    Returns
+    -------
+    bin_centers : ndarray, float
+        The q center of each bin.
+
+    bin_values : ndarray, int
+        The average intensity in the bin.
+    """
+    
+    if not (image.shape == q_values.shape):
+        raise ValueError('`image` and `q_values` must have the same shape')
+    if not (image.shape == mask.shape):
+        raise ValueError('`image` and `mask` must have the same shape')
+    
+    # figure out the number of bins to use
+    if n_bins != None:
+        bin_factor = float(n_bins) / radii.max()
+    else:
+        bin_factor = 25.0
+    
+    bin_assignments = np.floor( q_values * bin_factor ).astype(np.int32)
+    
+    weights = image.flatten() * mask.astype(np.int).flatten()
+    bin_values = np.bincount(bin_assignments.flatten(), weights=weights)
+    bin_values /= (np.bincount(bin_assignments.flatten() ) + 1e-100).astype(np.float)[:bin_values.shape[0]]
+    
+    bin_centers = np.arange(bin_values.shape[0]) / bin_factor
+    
+    return bin_centers, bin_values
