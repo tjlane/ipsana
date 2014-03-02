6f60cc2078ea1319667b5e510a8d0726a2b6490a
tjlane <thomas.joseph.lane@gmail.com>
2014-03-02 09:17:49 -0800

    used mate, so much nicer than vim

--- minitti_live.py
+++ minitti_live.py
@@ -93,132 +93,129 @@ while True:
         n_laser_on_shots  = 0
         n_laser_off_shots = 0
 
-shot_result = 'no data'
-t0 = time.time()
-s0 = 0 # shot counter
-
-n_q_bins = 100
-ra = RadialAverager(geometry, mask, n_bins=n_q_bins)
-
-for i,evt in enumerate(ds.events()):
-    
-    EVR_code = evt.get(psana.EvrData.DataV3, evr_src)
-    if EVR_code is None:
-        continue
-        
-    cspad = evt.get(psana.CsPad.DataV2, cspad_src)
-    corrected_image = evt.get(psana.ndarray_float32_3, cspad_src, 'calibrated_ndarr')
-    raw_image       = np.vstack([ cspad.quads(i).data() for i in range(4) ])
-
-    assert raw_image.shape == (32, 185, 194*2)
-    assert corrected_image.shape == (32, 185, 194*2)
-
-    xfel_status, uv_status = pumpprobe_status(EVR_code)
-    
-    # === filter for bad shots ===
-    
-    # filter 
-    if corrected_image.sum() < args.threshold:
-        print "-- bad shot :: intensity too low"
-        continue
-
-    # === extract timetool stamp ===
-
-    tau = epics.value('TTSPEC:FLTPOS_PS')
-    angle_shift = epics.value('LAS:FS3:REG:Angle:Shift:rd')
-
-    # === accumulate the results ===
-
-    if (xfel_status and uv_status):       # good data, w/ pump
-        shot_result = 'uv & xfel on'
-        n_laser_on_shots += 1
-        update_average(n_laser_on_shots, laser_on, corrected_image)
-        
-    elif (xfel_status and not uv_status): # good data, no pump
-        shot_result = 'uv off, xfel on'
-        n_laser_off_shots += 1
-        update_average(n_laser_off_shots, laser_off, corrected_image)
-        
-    else:                                 # no scattering data...
-        shot_result = 'xfel off'
-
-
-    # === ANALYZE AND BROADCAST ===
-
-    if ((n_laser_on_shots + n_laser_off_shots) % update_frequency == 0) \
-        and (n_laser_on_shots > 0):
-
-        # === compute radial averages ===
-        evt_rad     = ra(corrected_image)
-        avg_rad_on  = ra(laser_on)
-        avg_rad_off = ra(laser_off)
-
-        # normalize from 0.5 to 3.5 inv ang
-        n_evt_rad     = normalize(ra.bin_centers, evt_rad)
-        n_avg_rad_on  = normalize(ra.bin_centers, avg_rad_on)
-        n_avg_rad_off = normalize(ra.bin_centers, avg_rad_off)
-
-        # --- any data massaging for laser on/off ---
-        laser_onoff_diff     = n_avg_rad_on - n_avg_rad_off
-        laser_onoff_percdiff = laser_onoff_diff / n_avg_rad_on
-        Iq_sqrd = n_avg_rad_on * np.square(ra.bin_centers)
-
-        # ==============================
-        # PUBLISH DATA VIA ZMQ    
-        # ==============================
-        rad_evt_plot = IqPlotData(n_laser_off_shots + n_laser_on_shots,
-                                "I(q) Instantaneous",
-                                 ra.bin_centers, evt_rad)
-        socket.send("radevt", zmq.SNDMORE)
-        socket.send_pyobj(rad_evt_plot)
-
-
-        plotdata = np.hstack((avg_rad_on[:,None], avg_rad_off[:,None]))
-        rad_avg_plot = IqPlotData(n_laser_off_shots + n_laser_on_shots,
-                                "I(q) Laser On & Off Averages",
-                                 ra.bin_centers, plotdata)
-        socket.send("radavg", zmq.SNDMORE)
-        socket.send_pyobj(rad_avg_plot)
-
-
-        rad_diff_plot = IqPlotData(n_laser_off_shots + n_laser_on_shots,
-                                "I(q) Laser On/Off Difference",
-                                 ra.bin_centers, laser_onoff_diff)
-        socket.send("raddiff", zmq.SNDMORE)
-        socket.send_pyobj(rad_diff_plot)
-
-
-        rad_diffperc_plot = IqPlotData(n_laser_off_shots + n_laser_on_shots,
-                                "I(q) Laser On/Off Difference (%)",
-                                 ra.bin_centers, laser_onoff_percdiff)
-        socket.send("radperc", zmq.SNDMORE)
-        socket.send_pyobj(rad_diffperc_plot)
-
-
-        rad_Iq_sqrd = IqPlotData(n_laser_off_shots + n_laser_on_shots,
-                                "I(q) * q^2 vs q",
-                                 ra.bin_centers, Iq_sqrd)
-        socket.send("kratky", zmq.SNDMORE)
-        socket.send_pyobj(rad_Iq_sqrd)
-
-
-        delta_t = time.time() - t0
+        shot_result = 'no data'
         t0 = time.time()
+        s0 = 0 # shot counter
 
-        shot_rate = float(shot_index - s0) / delta_t
-        s0 = shot_index
+        n_q_bins = 100
+        ra = RadialAverager(geometry, mask, n_bins=n_q_bins)
 
-        print '\t--> sending results (%d Hz)' % (shot_rate)
+        for i,evt in enumerate(ds.events()):
+    
+            EVR_code = evt.get(psana.EvrData.DataV3, evr_src)
+            if EVR_code is None:
+                continue
         
-    print "Run: %d | Shot %d | %s" % (args.run, shot_index, shot_result)
-    shot_index += 1
+            cspad = evt.get(psana.CsPad.DataV2, cspad_src)
+            corrected_image = evt.get(psana.ndarray_float32_3, cspad_src, 'calibrated_ndarr')
+            raw_image       = np.vstack([ cspad.quads(i).data() for i in range(4) ])
+
+            assert raw_image.shape == (32, 185, 194*2)
+            assert corrected_image.shape == (32, 185, 194*2)
+
+            xfel_status, uv_status = pumpprobe_status(EVR_code)
+    
+            # === filter for bad shots ===
+    
+            # filter 
+            if corrected_image.sum() < args.threshold:
+                print "-- bad shot :: intensity too low"
+                continue
+
+            # === extract timetool stamp ===
+
+            tau = epics.value('TTSPEC:FLTPOS_PS')
+            angle_shift = epics.value('LAS:FS3:REG:Angle:Shift:rd')
+
+            # === accumulate the results ===
+
+            if (xfel_status and uv_status):       # good data, w/ pump
+                shot_result = 'uv & xfel on'
+                n_laser_on_shots += 1
+                update_average(n_laser_on_shots, laser_on, corrected_image)
+        
+            elif (xfel_status and not uv_status): # good data, no pump
+                shot_result = 'uv off, xfel on'
+                n_laser_off_shots += 1
+                update_average(n_laser_off_shots, laser_off, corrected_image)
+        
+            else:                                 # no scattering data...
+                shot_result = 'xfel off'
 
 
-print '\nSaving final results...'
+            # === ANALYZE AND BROADCAST ===
 
-hdf = h5py.File('/reg/neh/home2/tjlane/analysis/xppb0114/averages/r%d_laser_avg.h5' % args.run, 'w')
-hdf['/q_values']  = ra.bin_centers
-hdf['/laser_on']  = avg_rad_on
-hdf['/laser_off'] = avg_rad_off
-hdf.close()
+            if ((n_laser_on_shots + n_laser_off_shots) % update_frequency == 0) \
+                and (n_laser_on_shots > 0):
+
+                # === compute radial averages ===
+                evt_rad     = ra(corrected_image)
+                avg_rad_on  = ra(laser_on)
+                avg_rad_off = ra(laser_off)
+
+                # normalize from 0.5 to 3.5 inv ang
+                n_evt_rad     = normalize(ra.bin_centers, evt_rad)
+                n_avg_rad_on  = normalize(ra.bin_centers, avg_rad_on)
+                n_avg_rad_off = normalize(ra.bin_centers, avg_rad_off)
+
+                # --- any data massaging for laser on/off ---
+                laser_onoff_diff     = n_avg_rad_on - n_avg_rad_off
+                laser_onoff_percdiff = laser_onoff_diff / n_avg_rad_on
+                Iq_sqrd = n_avg_rad_on * np.square(ra.bin_centers)
+
+                # ==============================
+                # PUBLISH DATA VIA ZMQ    
+                # ==============================
+                rad_evt_plot = IqPlotData(n_laser_off_shots + n_laser_on_shots,
+                                        "I(q) Instantaneous",
+                                         ra.bin_centers, evt_rad)
+                socket.send("radevt", zmq.SNDMORE)
+                socket.send_pyobj(rad_evt_plot)
+
+
+                plotdata = np.hstack((avg_rad_on[:,None], avg_rad_off[:,None]))
+                rad_avg_plot = IqPlotData(n_laser_off_shots + n_laser_on_shots,
+                                        "I(q) Laser On & Off Averages",
+                                         ra.bin_centers, plotdata)
+                socket.send("radavg", zmq.SNDMORE)
+                socket.send_pyobj(rad_avg_plot)
+
+
+                rad_diff_plot = IqPlotData(n_laser_off_shots + n_laser_on_shots,
+                                        "I(q) Laser On/Off Difference",
+                                         ra.bin_centers, laser_onoff_diff)
+                socket.send("raddiff", zmq.SNDMORE)
+                socket.send_pyobj(rad_diff_plot)
+
+
+                rad_diffperc_plot = IqPlotData(n_laser_off_shots + n_laser_on_shots,
+                                        "I(q) Laser On/Off Difference (%)",
+                                         ra.bin_centers, laser_onoff_percdiff)
+                socket.send("radperc", zmq.SNDMORE)
+                socket.send_pyobj(rad_diffperc_plot)
+
+
+                rad_Iq_sqrd = IqPlotData(n_laser_off_shots + n_laser_on_shots,
+                                        "I(q) * q^2 vs q",
+                                         ra.bin_centers, Iq_sqrd)
+                socket.send("kratky", zmq.SNDMORE)
+                socket.send_pyobj(rad_Iq_sqrd)
+
+
+                delta_t = time.time() - t0
+                t0 = time.time()
+
+                shot_rate = float(shot_index - s0) / delta_t
+                s0 = shot_index
+
+                print '\t--> sending results (%d Hz)' % (shot_rate)
+        
+            print "Run: %d | Shot %d | %s" % (args.run, shot_index, shot_result)
+            shot_index += 1
+            
+        except Exception as e:
+            print 'Got exception:', e
+            print 'Continuing WITHOUT FEAR!!!'
+            print ''
+
 
